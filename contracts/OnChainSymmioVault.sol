bdd69ef0a26c054f99804b5cca994d4cae1df078
Alison024 <andreo.ublin24@gmail.com>
2025-12-22 15:46:40 +0000

    fixed issue "SafeERC20's forceApprove is not used #6"

--- contracts/OnChainSymmioVault.sol
+++ contracts/OnChainSymmioVault.sol
@@ -91,10 +91,7 @@ contract OnChainSymmioVault is
         currentDeposit += amount;
         emit Deposit(_msgSender(), amount);
         
-        require(
-            collateralToken.approve(address(symmio), amount),
-            "SymmioSolverDepositor: Approve failed"
-        );
+        collateralToken.forceApprove(address(symmio), amount);
         symmio.depositFor(solver, amount);
         emit DepositToSymmio(_msgSender(), solver, amount);
     }
